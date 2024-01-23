d76eeb2b7e4d90528412549c5399df452386de5f
mark-fender <marek.lipcak@gmail.com>
2024-01-23 20:07:46 +0100

    new font, resolved responsivity problem

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { Inter } from 'next/font/google';
+import localFont from 'next/font/local';
 import { ReactNode } from 'react';
 import './globals.css';
 import ActiveSectionContextProvider from '../context/activeSectionContext';
@@ -8,7 +8,25 @@ import ThemeSwitcher from '../components/ThemeSwitcher';
 import { Toaster } from 'react-hot-toast';
 import Providers from './providers';
 
-const inter = Inter({ subsets: ['latin'] });
+const satoshi = localFont({
+  src: [
+    {
+      path: './fonts/Satoshi-Medium.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Satoshi-Italic.ttf',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: './fonts/Satoshi-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+});
 
 export const metadata = {
   title: 'Marek Lipčák',
@@ -19,24 +37,24 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <html lang='en' className='!scroll-smooth' suppressHydrationWarning>
       <body
-        className={`${inter.className}  bg-slate-200 text-gray-950 relative pt-28 sm:pt-36 
+        className={`${satoshi.className}  bg-slate-200 text-gray-950 relative pt-28 sm:pt-36 
       dark:bg-slate-900 dark:text-gray-50 dark:text-opacity-90`}>
         <div
-          className='bg-[#a6afec] absolute top-[0] -z-10 right-[15rem] h-[31.25rem] w-[50em] rounded-full blur-[15rem] 
-          sm:w-[68.75rem] xl:right-[-5rem] 2xl:right-[15rem] dark:bg-[#5b639e]'
+          className='bg-[#a6afec] absolute top-[-6rem] -z-10 right-[11rem] 2xl:right-[25rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[15rem] 
+          sm:w-[68.75rem] dark:bg-[#5b639e]'
         />
         <div
-          className='bg-[#9fddf4] absolute top-[10rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[15rem] 
-          sm:w-[90rem] md:left-[-55rem] lg:left-[-30rem] xl:left-[-25rem] 2xl:left-0 dark:bg-[#70a3b6]'
+          className='bg-[#9fddf4] absolute top-[-1rem] -z-10 left-[-50rem] h-[31.25rem] w-[50rem] rounded-full blur-[15rem] 
+          sm:w-[68.75rem] sm:left-[-60rem] md:left-[-50rem] lg:left-[-35rem] xl:left-[-25rem] 2xl:left-[-5rem] dark:bg-[#70a3b6]'
         />
 
         <div
-          className='bg-[#cff1fd] absolute bottom-[0rem] -z-10 right-[5rem] xl:right-[15rem] h-[15rem] w-[25em] rounded-full blur-[15rem] 
-          sm:w-[68.75rem] dark:bg-[#70a3b6]'
+          className='bg-[#a6afec] absolute bottom-[-2rem] -z-10 right-[11rem] 2xl:right-[25rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[15rem] 
+          sm:w-[68.75rem] dark:bg-[#5b639e]'
         />
         <div
-          className='bg-[#cdd3f8] absolute bottom-[5rem] -z-10 left-[-5rem] h-[15rem] w-[25rem] rounded-full blur-[15rem] 
-          sm:w-[90rem] md:left-[-35rem] lg:left-[-30rem] xl:left-[15rem] dark:bg-[#5b639e]'
+          className='bg-[#9fddf4] absolute bottom-[2rem] -z-10 left-[-50rem] h-[31.25rem] w-[50rem] rounded-full blur-[15rem] 
+          sm:w-[68.75rem] sm:left-[-60rem] md:left-[-50rem] lg:left-[-35rem] xl:left-[-25rem] 2xl:left-[-5rem] dark:bg-[#70a3b6]'
         />
         <Providers>
           <ActiveSectionContextProvider>

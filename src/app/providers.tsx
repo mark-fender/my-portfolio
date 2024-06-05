f7b61bfaa64f257e09ec1bae93c89c5c39ca8d71
mark-fender <marek.lipcak@gmail.com>
2024-06-05 16:21:01 +0200

    default theme

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,14 +4,12 @@ import ActiveSectionContextProvider from '@/context/activeSectionContext';
 import { ThemeProvider } from 'next-themes';
 import { ReactNode } from 'react';
 
-const Providers = ({ children }: { children: ReactNode }) => {
-  return (
-    <ActiveSectionContextProvider>
-      <ThemeProvider attribute='class' defaultTheme='dark' enableSystem>
-        {children}
-      </ThemeProvider>
-    </ActiveSectionContextProvider>
-  );
-};
+const Providers = ({ children }: { children: ReactNode }) => (
+  <ActiveSectionContextProvider>
+    <ThemeProvider attribute='class' defaultTheme='dark' enableSystem>
+      {children}
+    </ThemeProvider>
+  </ActiveSectionContextProvider>
+);
 
 export default Providers;
